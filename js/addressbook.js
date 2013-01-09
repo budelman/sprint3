bc868b9296fa54f8d5241d9572f5ec2bed1f531c
Mat Budelman <mattbudelman@gmail.com>
2013-01-09 16:57:40 -0500

    condensed code separated getJSON from addr and search functions

--- js/addressbook.js
+++ js/addressbook.js
@@ -1,14 +1,4 @@
-
-/* wrap everything in an anonymous function to contain the variables. This executes the address book*/
-
-$(document).ready(function() {
-	
-	/* define the DOM elements and common variables you'll need */
-    var searchForm = $("#search-form"),
-        searchField = $("#q"),
-        target = $("#output");
-            
-    var addr = {
+var addr = {
         
         search : function(event){
             
@@ -17,49 +7,63 @@ $(document).ready(function() {
             
             // stop the default behavior
             event.preventDefault();
-            
-            /* start the Ajax call */
-            $.getJSON('data/contacts.json', function (data) {
-				
-                // save the input value, contacts length and i to variables
-                var searchValue = searchField.value,
-                    addrBook = data.addressBook,
-                    count = addrBook.length,
-                    i;
+
+        } // end search method
+
+}; // end addr function
+
+/* ready the address book and execute the JSON data. */
+
+$(document).ready(function() {
+	
+    /* start the Ajax call */
+    $.getJSON('data/contacts.json', function (data) {
+
+    // save the input value, contacts length and i to variables
+    var searchValue = searchField.value,
+        addrBook = data.addressBook,
+        count = addrBook.length,
+        i;
                     
-                // clear the target area just incase there's something in it.
-                $('#output').empty();
+    // clear the target area just incase there's something in it.
+    $('#output').empty();
+
+        // check the count, of course
+        if(count > 0 && searchValue !== ""){
+        
+            // loop through the contacts
+            $.each(addrBook, function (i, obj) {
+            
+                // look through the name value to see if it contains the searchterm string
+                var obj = addrBook[i],
+                    isItFound = obj.name.indexOf(searchValue);
+
+                // anything other than -1 means we found a match
+                if(isItFound !== -1) {
+                    
+                    $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a></p>').hide().fadeIn();
+                    
+                } // end if
+
+            }); // end .each loop
+
+        } // end count check
+
+    }); // end getJSON call
+    
+}); // end document.ready function
+
+
+
+
+
+
+    
                             
                 // activate auto complete on keyUp
                 $('#q').keyup(function(event) {
                     addr.search(event);
                 });
                 
-                // check the count, of course
-                if(count > 0 && searchValue !== ""){
+
                 
-                    // loop through the contacts
-                    $.each(addrBook, function (i, obj) {
-                    
-                        // look through the name value to see if it contains the searchterm string
-                        var obj = addrBook[i],
-                            isItFound = obj.name.indexOf(searchValue);
-    
-                        // anything other than -1 means we found a match
-                        if(isItFound !== -1) {
-                            
-                            $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a></p>').hide().fadeIn();
-                            
-                        } // end if
-    
-                    }); // end .each loop
-    
-                } // end count check
-                
-            }); // end getJSON call
-
-        } // end Search method
-
-    } // end addr function
-
-})(); // end anonymous function
