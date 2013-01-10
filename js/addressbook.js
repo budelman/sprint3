4e75835fafeb800c3ae95292af54a9c0decee26c
Mat Budelman <mattbudelman@gmail.com>
2013-01-10 14:53:49 -0500

    testing a lot via console.log

--- js/addressbook.js
+++ js/addressbook.js
@@ -9,48 +9,54 @@ $(document).ready(function() {
     // save the input value, contacts length and i to variables
     var addrBook = data.addressBook,
         count = addrBook.length,
-        searchValue = searchField.value,
         i;
 
     var addr = {
         
-    search : function(event){
+        search : function(event){
             
-        // stop the default behavior
-        event.preventDefault();
-
-        // check the count, of course
-        if(count > 0 && searchValue !== ""){
-        
+            console.log("the search was accessed!");
+            
+            // take the value of the search field and return it to all lowercase and then set it so we can check it later for content.
+            var searchValue = searchField.value.toLowerCase();
+            
+            // empty the output field before we dump things in!
             $('#output').empty();
+            console.log("We emptied the field!")
+  
+            // stop the default behavior
+            event.preventDefault();
+    
+            // check the count, of course
+            if(count > 0 && searchValue !== ""){
             
-            // loop through the contacts
-            $.each(addrBook, function (i, obj) {
-            
-                // look through the name value to see if it contains the searchterm string
-                var obj = addrBook[i],
-                    isItFound = obj.name.indexOf(searchValue);
-
-                // anything other than -1 means we found a match
-                if(isItFound !== -1) {
-                    
-                    $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a></p>').hide().fadeIn();
-                    
-                } // end if
-
-            }); // end .each loop
-
-        } // end count check
-
-    } // end search function
+                // loop through the contacts
+                $.each(addrBook, function (i, obj) {
+                
+                    // look through the name value to see if it contains the searchterm string
+                    var obj = addrBook[i],
+                        isItFound = obj.name..indexOf(searchValue);
+    
+                    // anything other than -1 means we found a match
+                    if(isItFound !== -1) {
+                        
+                        $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a></p>').hide().fadeIn();
+                        
+                    } // end if
+    
+                }); // end .each loop
+    
+            } // end count check
+    
+        } // end search function
     
     }; // end addr object
-    }); // end json
+    // activate auto complete on keyUp
+    $('#q').keyup(function(event) {
+        addr.search(event);
+        console.log("the key-up worked!!!");
+    });
 
-// activate auto complete on keyUp
-$('#q').keyup(function(event) {
-    addr.search(event);
-    console.log("the key-up worked!!!");
-});
+}); // end json
 
 }); // end document.ready function
